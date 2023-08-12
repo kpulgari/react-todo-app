8ea0a5c0a779d3dcf0fe994fefec31d305662194
Kaushik Pulgari <90290549+kpulgari@users.noreply.github.com>
2023-08-12 00:05:37 -0500

    Remove documentation

--- src/SubmitTask.tsx
+++ src/SubmitTask.tsx
@@ -14,20 +14,6 @@ interface Props {
   onClearListClick: () => void;
 }
 
-/**
- * The `SubmitTask` component is a form that allows users to input a task, submit it, and clear the task list.
- *
- * @param {Props} props - The props for the `SubmitTask` component.
- *   - `children` (string): The placeholder text for the input field.
- *   - `submitBarId` (string): The HTML ID attribute for the input element.
- *   - `submitBarName` (string): The HTML name attribute for the input element.
- *   - `submitButtonId` (string): The HTML ID attribute for the submit button.
- *   - `clearButtonId` (string): The HTML ID attribute for the clear button.
- *   - `onSubmitClick` (function): Callback function to handle task submission.
- *   - `onClearListClick` (function): Callback function to clear the task list.
- *
- * @returns The `SubmitTask` component.
- */
 export const SubmitTask = ({
   children,
   submitBarId,
