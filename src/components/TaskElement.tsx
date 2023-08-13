21e24d18b622cdac118a18ca800c2db152480ee0
Kaushik Pulgari <kpulg2@illinois.edu>
2023-08-12 23:36:10 -0500

    Add new highlight button

--- src/components/TaskElement.tsx
+++ src/components/TaskElement.tsx
@@ -1,44 +1,95 @@
-import React, { useState } from "react";
-import "../styles/TaskElement.css";
+import React, { ReactNode, useEffect, useState } from "react";
+import { addButtonHeldListeners } from "../utils/buttonHeld";
 
 interface Props {
   children: string;
   id: string;
+  doneIcon: ReactNode;
+  notDoneIcon: ReactNode;
+  removeIcon: ReactNode;
+  highlightIcon: ReactNode;
+  notHighlightIcon: ReactNode;
 }
 
-export const TaskElement = ({ children, id }: Props) => {
-  const [taskDone, setTaskDone] = useState(false);
+export const TaskElement = ({
+  children,
+  id,
+  doneIcon,
+  notDoneIcon,
+  removeIcon,
+  highlightIcon,
+  notHighlightIcon,
+}: Props) => {
+  const [crossTask, setCrossTask] = useState(false);
   const [removeTask, setRemoveTask] = useState(false);
+  const [highlightTask, setHighlightTask] = useState(false);
+
+  const doneButtonId = `task-complete-button-${id}`;
+  const removeButtonId = `task-remove-button-${id}`;
+  const highlightButtonId = `task-highlight-button-${id}`;
 
   const handleOnCompleteClick = () => {
-    setTaskDone(!taskDone);
+    setCrossTask(!crossTask);
   };
 
   const handleOnRemoveClick = () => {
     setRemoveTask(true);
   };
 
+  const handleOnHighlightClick = () => {
+    setHighlightTask(!highlightTask);
+    console.log(highlightTask);
+  };
+
+  useEffect(() => {
+    const cleanupCompleteButton = addButtonHeldListeners(doneButtonId);
+    const cleanupRemoveButton = addButtonHeldListeners(removeButtonId);
+    const cleanupHighlightButton = addButtonHeldListeners(highlightButtonId);
+
+    return () => {
+      cleanupCompleteButton();
+      cleanupRemoveButton();
+      cleanupHighlightButton();
+    };
+  }, [doneButtonId, removeButtonId, highlightButtonId]);
+
   return removeTask ? null : (
-    <li
-      className={`task-element${taskDone ? "-done" : ""}`}
-      id={`task-element-${id}`}
-    >
-      {children}
+    <li className="task-element" id={`task-element-${id}`}>
+      <span
+        className={`task-text ${crossTask ? "crossed" : ""} ${
+          highlightTask ? "highlight" : ""
+        }`}
+      >
+        {children}
+      </span>
       <button
-        id={`task-complete-button-${id}`}
+        className={`submit-button submit task ${crossTask ? "clicked" : ""}`}
+        id={doneButtonId}
         onClick={() => {
           handleOnCompleteClick();
         }}
       >
-        {taskDone ? "Not Done" : "Done"}
+        {!crossTask ? doneIcon : notDoneIcon}
       </button>
       <button
-        id={`task-remove-button-${id}`}
+        className={`submit-button highlight task ${
+          highlightTask ? "clicked" : ""
+        }`}
+        id={highlightButtonId}
+        onClick={() => {
+          handleOnHighlightClick();
+        }}
+      >
+        {!highlightTask ? highlightIcon : notHighlightIcon}
+      </button>
+      <button
+        className="submit-button remove task"
+        id={removeButtonId}
         onClick={() => {
           handleOnRemoveClick();
         }}
       >
-        Remove
+        {removeIcon}
       </button>
     </li>
   );
